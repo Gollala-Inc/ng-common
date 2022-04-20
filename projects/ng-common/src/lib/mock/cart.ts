532ac146aa87127cf0adaa5738f5e7dd913864ce
Taesung Yun <cheonyulin@gmail.com>
2022-04-20 22:01:28 +0900

    [mason] create mock-data in cart

---
+++ projects/ng-common/src/lib/mock/cart.ts
@@ -0,0 +1,118 @@
+import {Cart} from "../interface/cart.model";
+
+export const cart: Cart = {
+  _id: '61fcd7f6063293a036599d18',
+  customer: '61fcd7f6063293a036599d15',
+  items: [
+    {
+      options: {
+        color: '아이보리',
+        size: '3XL'
+      },
+      product: '62454df0af08500006f416b8',
+      productName: '대표번호2테스트',
+      quantity: 2,
+      _id: '625e7ce8a9ecbcf44b4ba460',
+      updatedAt: '2022-04-19T09:12:08.704Z',
+      createdAt: '2022-04-19T09:12:08.704Z'
+    },
+    {
+      options: {
+        color: '블랙',
+        size: 'FREE'
+      },
+      product: '6041c3f229d8210007d2c4fe',
+      productName: '배',
+      quantity: 2,
+      _id: '625fffd974a57ba6ae46a1a8',
+      updatedAt: '2022-04-20T12:43:05.230Z',
+      createdAt: '2022-04-20T12:43:05.230Z'
+    },
+    {
+      options: {
+        color: '스카이블루',
+        size: '88'
+      },
+      product: '620c85e9cb17b30006cd6750',
+      productName: '테스트',
+      quantity: 1,
+      _id: '625fffec74a57ba6ae46a1c1',
+      updatedAt: '2022-04-20T12:43:24.404Z',
+      createdAt: '2022-04-20T12:43:24.404Z'
+    },
+    {
+      options: {
+        color: '스카이블루',
+        size: 'FREE'
+      },
+      product: '620c85e9cb17b30006cd6750',
+      productName: '테스트',
+      quantity: 1,
+      _id: '625fffec74a57ba6ae46a1c6',
+      updatedAt: '2022-04-20T12:43:24.419Z',
+      createdAt: '2022-04-20T12:43:24.419Z'
+    },
+    {
+      options: {
+        color: '스카이블루',
+        size: 'S'
+      },
+      product: '620c85e9cb17b30006cd6750',
+      productName: '테스트',
+      quantity: 1,
+      _id: '625fffec74a57ba6ae46a1cc',
+      updatedAt: '2022-04-20T12:43:24.427Z',
+      createdAt: '2022-04-20T12:43:24.427Z'
+    },
+    {
+      options: {
+        color: '옐로우',
+        size: 'XL'
+      },
+      product: '6243cc8592c19c000658323b',
+      productName: '테스트용',
+      quantity: 1,
+      _id: '625ffffb74a57ba6ae46a1f1',
+      updatedAt: '2022-04-20T12:43:39.617Z',
+      createdAt: '2022-04-20T12:43:39.617Z'
+    },
+    {
+      options: {
+        color: '머스타드',
+        size: '2XL'
+      },
+      product: '6243cc8592c19c000658323b',
+      productName: '테스트용',
+      quantity: 1,
+      _id: '625ffffb74a57ba6ae46a1f9',
+      updatedAt: '2022-04-20T12:43:39.641Z',
+      createdAt: '2022-04-20T12:43:39.641Z'
+    },
+    {
+      options: {
+        color: '블랙',
+        size: 'FREE'
+      },
+      product: '6243cc2192c19c000658323a',
+      productName: '데브테스트',
+      quantity: 1,
+      _id: '6260000274a57ba6ae46a224',
+      updatedAt: '2022-04-20T12:43:46.596Z',
+      createdAt: '2022-04-20T12:43:46.596Z'
+    },
+    {
+      options: {
+        color: '차콜',
+        size: 'FREE'
+      },
+      product: '6243cc2192c19c000658323a',
+      productName: '데브테스트',
+      quantity: 1,
+      _id: '6260000274a57ba6ae46a22e',
+      updatedAt: '2022-04-20T12:43:46.609Z',
+      createdAt: '2022-04-20T12:43:46.609Z'
+    }
+  ],
+  createdAt: '2022-02-04T07:38:30.530Z',
+  updatedAt: '2022-04-20T12:43:46.609Z'
+}
