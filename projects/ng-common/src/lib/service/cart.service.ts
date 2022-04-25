215d3e00f908a8e80f3ac5bdc77af35a890241d6
Taesung Yun <cheonyulin@gmail.com>
2022-04-25 15:39:24 +0900

    Revert "[mason] restore cusatomCartId, cartId"
    This reverts commit b396d26f

--- projects/ng-common/src/lib/service/cart.service.ts
+++ projects/ng-common/src/lib/service/cart.service.ts
@@ -9,9 +9,6 @@ import {BehaviorSubject} from "rxjs";
   providedIn: 'root'
 })
 export class CartService {
-  private _customCartId!: string;
-  private _cartId!: string;
-
   cartInfo$: BehaviorSubject<CartInfo> =  new BehaviorSubject<CartInfo>({
     products: [],
     excels: [],
@@ -20,14 +17,6 @@ export class CartService {
     totalCnt: 0
   });
   cartCounts$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-
-  get customCartId() {
-    return this._customCartId;
-  }
-
-  get cartId() {
-    return this._cartId;
-  }
 
   get cartInfo() {
     return this.cartInfo$.getValue();
@@ -97,7 +86,6 @@ export class CartService {
       }),
       mergeMap(cartDoc => {
         cartItems = cartDoc.items;
-        this._cartId = cartDoc._id;
         const productIds = cartDoc.items.map((cartItem: { product: any; }) => cartItem.product);
         return this.requestProductList(productIds);
       }),
@@ -106,7 +94,6 @@ export class CartService {
         return this.getAuthExcelCart();
       })
     ).subscribe(((customCartInfo: any) => {
-      this._customCartId = customCartInfo._id;
       this.cartCounts$.next(productsCnt + customCartInfo.items.length);
     }))
   }
