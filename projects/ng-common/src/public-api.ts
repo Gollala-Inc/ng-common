db806eac721ea3ad154085bab76b6a164dd6a468
Taesung Yun <cheonyulin@gmail.com>
2022-05-04 11:35:02 +0900

    [mason] add favorite service in public-api.ts

--- projects/ng-common/src/public-api.ts
+++ projects/ng-common/src/public-api.ts
@@ -17,6 +17,7 @@ export * from './lib/service/security-service.service';
 export * from './lib/service/cart.service';
 export * from './lib/service/order.service';
 export * from './lib/service/router.service';
+export * from './lib/service/favorite.service';
 export * from './lib/component/login-dialog/login-dialog.component';
 export * from './lib/page/signup-page/signup-page.component';
 export * from './lib/component/naver-script/naver-script.component';
