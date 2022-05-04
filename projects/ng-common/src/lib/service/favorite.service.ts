6f515e3b00ce3a8deb0216d79c65af00949bcdf8
Taesung Yun <cheonyulin@gmail.com>
2022-05-04 11:34:36 +0900

    [mason] add favorite service

---
+++ projects/ng-common/src/lib/service/favorite.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {RestService} from "./rest.service";
+import {FavoriteTypes, MyFavorite} from "../interface/favorite.model";
+import {Observable} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  constructor(
+    private restService: RestService
+  ) { }
+
+  public getPersonalFavorites(): Observable<MyFavorite[]> {
+    return this.restService.GET('https://commerce-api.gollala.org/favorite/auth/', {
+      handleError: true
+    });
+  }
+
+  public getPersonalFavorite(id: string): Observable<MyFavorite> {
+    return this.restService.GET(`https://commerce-api.gollala.org/favorite/auth/${id}`, {
+      handleError: true
+    });
+  }
+
+  public addPersonalFavorites(body: {type: FavoriteTypes; targetId: string}) {
+    return this.restService.POST('https://commerce-api.gollala.org/favorite/auth', {
+      body,
+      handleError: true
+    });
+  }
+}
